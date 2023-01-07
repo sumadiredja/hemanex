8c6f6b8d8ab2525ab49bf16990858c9ec6f3933e
hamdanifadhli <hamdanifadhli@gmail.com>
2023-01-07 22:22:44 +0700

    autocommit

---
+++ menu/nexus_menu.go
@@ -0,0 +1,159 @@
+package menu
+
+import (
+	"fmt"
+
+	"github.com/sumadiredja/hemanex/registry"
+
+	helper "github.com/sumadiredja/hemanex/helper"
+
+	"github.com/urfave/cli"
+)
+
+func ListImages(c *cli.Context) error {
+	r, err := registry.NewRegistry(c)
+	if err != nil {
+		return helper.CliErrorGen(err, 1)
+	}
+	images, err := r.ListImages()
+	if err != nil {
+		return helper.CliErrorGen(err, 1)
+	}
+	for _, image := range images {
+		fmt.Println(image)
+	}
+	helper.CliInfoVerbose(fmt.Sprintf("Total images: %d\n", len(images)))
+	return nil
+}
+
+func ListTagsByImage(c *cli.Context) error {
+	var imgName = c.String("name")
+	r, err := registry.NewRegistry(c)
+	if err != nil {
+		return helper.CliErrorGen(err, 1)
+	}
+	if imgName == "" {
+		return helper.ShowSubCommand("please provide image name", c)
+	}
+	tags, err := r.ListTagsByImage(imgName)
+
+	compareStringNumber := func(str1, str2 string) bool {
+		return helper.ExtractNumberFromString(str1) < helper.ExtractNumberFromString(str2)
+	}
+	helper.Compare(compareStringNumber).Sort(tags)
+
+	if err != nil {
+		return helper.CliErrorGen(err, 1)
+	}
+	for _, tag := range tags {
+		fmt.Println(tag)
+	}
+	helper.CliInfoVerbose(fmt.Sprintf("There are %d images for %s\n", len(tags), imgName))
+	return nil
+}
+
+func ShowImageInfo(c *cli.Context) error {
+	var imgName = c.String("name")
+	var tag = c.String("tag")
+	r, err := registry.NewRegistry(c)
+	if err != nil {
+		return helper.CliErrorGen(err, 1)
+	}
+	if imgName == "" || tag == "" {
+		return helper.ShowSubCommand("please provide image name and tag", c)
+	}
+	manifest, err := r.ImageManifest(imgName, tag)
+	if err != nil {
+		return helper.CliErrorGen(err, 1)
+	}
+	fmt.Printf("Image: %s:%s\n", imgName, tag)
+	fmt.Printf("Size: %d\n", manifest.Config.Size)
+	fmt.Println("Layers:")
+	for _, layer := range manifest.Layers {
+		fmt.Printf("\t%s\t%d\n", layer.Digest, layer.Size)
+	}
+	return nil
+}
+
+func DeleteImage(c *cli.Context) error {
+	var imgName = c.String("name")
+	var tag = c.String("tag")
+	var keep = c.Int("keep")
+	if imgName == "" {
+		return helper.ShowSubCommand("please provide image name", c)
+	} else {
+		r, err := registry.NewRegistry(c)
+		if err != nil {
+			return helper.CliErrorGen(err, 1)
+		}
+		if tag == "" {
+			if keep == 0 {
+				return helper.ShowSubCommand("please provide image tag or how many images you want to keep", c)
+			} else {
+				tags, err := r.ListTagsByImage(imgName)
+				compareStringNumber := func(str1, str2 string) bool {
+					return helper.ExtractNumberFromString(str1) < helper.ExtractNumberFromString(str2)
+				}
+				helper.Compare(compareStringNumber).Sort(tags)
+				if err != nil {
+					return helper.CliErrorGen(err, 1)
+				}
+				if len(tags) >= keep {
+					for _, tag := range tags[:len(tags)-keep] {
+						fmt.Printf("%s:%s image will be deleted ...\n", imgName, tag)
+						err = r.DeleteImageByTag(imgName, tag)
+						if err != nil {
+							return helper.CliErrorGen(err, 1)
+						}
+					}
+				} else {
+					fmt.Printf("Only %d images are available\n", len(tags))
+				}
+			}
+		} else {
+			err = r.DeleteImageByTag(imgName, tag)
+			if err != nil {
+				return helper.CliErrorGen(err, 1)
+			}
+		}
+	}
+	return nil
+}
+
+func ShowTotalImageSize(c *cli.Context) error {
+	var imgName = c.String("name")
+	var totalSize (int64) = 0
+
+	if imgName == "" {
+		return helper.ShowSubCommand("please provide image name", c)
+	} else {
+		r, err := registry.NewRegistry(c)
+		if err != nil {
+			return helper.CliErrorGen(err, 1)
+		}
+
+		tags, err := r.ListTagsByImage(imgName)
+		if err != nil {
+			return helper.CliErrorGen(err, 1)
+		}
+
+		for _, tag := range tags {
+			manifest, err := r.ImageManifest(imgName, tag)
+			if err != nil {
+				return helper.CliErrorGen(err, 1)
+			}
+
+			sizeInfo := make(map[string]int64)
+
+			for _, layer := range manifest.Layers {
+				sizeInfo[layer.Digest] = layer.Size
+			}
+
+			for _, size := range sizeInfo {
+				totalSize += size
+			}
+		}
+		fmt.Printf("%d %s\n", totalSize, imgName)
+	}
+	return nil
+}
