be87ea7d9bd0f7f1cb0d0ceb438392dbec08ebc5
hamdanifadhli <hamdanifadhli@gmail.com>
2023-01-08 11:15:01 +0700

    autocommit

---
+++ docs/src/layouts/default/AppBar.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app-bar :elevation="2" :collapse="globalStore.isCollapse()">
+    <v-app-bar-title>Hemanex CLI</v-app-bar-title>
+
+    <v-spacer></v-spacer>
+    <template v-slot:append>
+      <v-btn icon="mdi-menu" @click="globalStore.changeCollapse()"></v-btn>
+
+      <v-btn icon="mdi-brightness-4" @click="changeTheme()"></v-btn>
+    </template>
+  </v-app-bar>
+</template>
+
+<script lang="ts" setup>
+import { useTheme } from "vuetify/lib/framework.mjs";
+import { useGlobalStore } from "../../store/app";
+const globalStore = useGlobalStore();
+const theme = useTheme();
+
+function changeTheme() {
+  theme.global.name.value = theme.global.current.value.dark ? "light" : "dark";
+}
+</script>
